ec09ed385145ff5f0cc3bee07fd5aa8bdeec3e17
hbrashid <hbrashid@gmail.com>
2020-06-09 23:53:28 -0500

    some data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+
+class Data extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+
+	render() {
+		return (
+			<div style={{
+        display: "flex",
+        flexDirection: "column",
+        textAlign: "center",
+        padding: "10px"
+      }}>
+				<div>
+          <div style={{
+            fontWeight: "bold"}} >
+            {this.props.info.Country}</div>
+        </div>
+          <p>Total Confirmed: {this.props.info.TotalConfirmed}</p>
+          <p>Total Deaths: {this.props.info.TotalDeaths}</p>
+			</div>
+		);
+	}
 }
 
-export default App;
+
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      hits: [{}]
+     };
+  }
+
+
+componentDidMount() {
+  this.fetchData();
+}
+  
+
+
+fetchData() {
+  fetch(
+    `https://api.covid19api.com/summary`
+		)
+    .then((json) => json.json())
+    .then((data) => {
+      this.setState({
+        hits: data.Countries
+      });
+      console.log(data);
+    })
+    .catch((error) => console.log("parsing failed", error));
+  }
+    
+    
+    render() { 
+      return (  
+        <div>
+        
+          {this.state.hits.map((covidData, index) => (
+					<Data key={index} info={covidData} /> ))}
+        
+        </div>
+        )
+      }
+  }
+
+  
+  export default App;
+  
