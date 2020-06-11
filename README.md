66455f0de3ffc4b915178bdbee7a797105848621
crashdaddy <kennethscoggins@mail.com>
2020-06-10 21:42:20 -0500

    added country flags

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ I installed amCharts to provide this heat map functionality.
 If you want to use it, you'll have to:
 
 npm install @amcharts/amcharts4
+
 npm install @amcharts/amcharts4-geodata
 
 before it'll work
--- src/App.js
+++ src/App.js
@@ -18,7 +18,8 @@ function Attributions() {
   return(
     <div className="attributions">
       &nbsp;API by: <a href="https://covid19api.com/" target="blank">Covid-19 API</a><br/>
-      &nbsp;Data Provided By: <a href="https://github.com/CSSEGISandData/COVID-19" target="blank">COVID-19 Data Repository by the Center for Systems Science and Engineering (CSSE) at Johns Hopkins University</a>
+      &nbsp;Data Provided By: <a href="https://github.com/CSSEGISandData/COVID-19" target="blank">COVID-19 Data Repository by the Center for Systems Science and Engineering (CSSE) at Johns Hopkins University</a><br/>
+      &nbsp;Country Flags Provided By: <a href="https://www.countryflags.io/" target="blank">Country Flags API</a>
     </div>
   )
 }
@@ -35,12 +36,8 @@ class Data extends Component {
     return (
       <div className="dataDiv">
         <div>
-          <div
-            style={{
-              fontWeight: "bold",
-            }}
-          >
-            {this.props.info.Country}
+          <div style={{fontWeight: "bold"}}>
+           <img src={`https://www.countryflags.io/${this.props.info.CountryCode}/shiny/24.png`} style={{float:'left',marginRight:'5px'}} /> {this.props.info.Country}
           </div>
         </div>
         <div>
